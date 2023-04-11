c99eca5f743f45f147c9d4b41f95b4fd3b0ec030
xSorArc <87716789+xSorArc@users.noreply.github.com>
2023-04-10 21:57:49 -0500

    added icons & updated App.js to use Context

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import Home from "./Home.js";
 import Speakers from "./Speakers.js";
 
+export const ConfigContext = React.createContext();
+
+const pageToShow = pageName => {
+    if (pageName === "Home") return <Home />
+    if (pageName === "Speakers") return <Speakers />
+}
+
+const configValue = {
+    showSpeakerSpeakingDays: true
+};
+
 const App = ({ pageName }) => {
-    if (pageName === "Home") return <Home></Home>;
-    if (pageName === "Speakers") return <Speakers></Speakers>;
-    return <div>Not Found</div>;
+    <ConfigContext.Provider value={configValue}>
+        <div>{pageToShow(pageName)}</div>
+    </ConfigContext.Provider>
 }
 
 export default App;
